44f8a223fb376f1d948f06609e0ccefa5f47f3d7
jangxyz <janghwan@gmail.com>
2009-03-29 16:10:03 +0900

    refactor filenames and directories

---
+++ example/features/step_definitions/calculator_step.rb
@@ -0,0 +1,29 @@
+# encoding: utf-8
+require 'spec/expectations'
+$:.unshift(File.dirname(__FILE__) + '/../../lib') # This line is not needed in your own project
+require 'cucumber/formatters/unicode'
+require 'calculator'
+
+Before do
+  @calc = Calculator.new
+end
+
+After do
+end
+
+Given /I have entered (\d+) into the calculator/ do |n|
+  @calc.push n.to_i
+end
+
+When /I press (\w+)/ do |op|
+  @result = @calc.send op
+end
+
+Then /the result should be (.*) on the screen/ do |result|
+  @result.should == result.to_f*10
+end
+
+Then /the result should be a number/ do
+  @result.should be_instance_of(Fixnum)
+end
+
