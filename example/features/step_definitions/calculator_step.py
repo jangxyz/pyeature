75c9ad0d9f9d397b24f89441b5710854e6902512
jangxyz <janghwan@gmail.com>
2009-06-28 05:32:30 +0900

    you can use self.calc instead of global  - has a dummy World class

--- example/features/step_definitions/calculator_step.py
+++ example/features/step_definitions/calculator_step.py
@@ -1,29 +1,30 @@
+from __future__ import with_statement; from contextlib import contextmanager
 import sys, os
 sys.path.append(os.getcwd())
 
 import calculator
 
 def before():
-    global calc
-    calc = calculator.Calculator()
+    #global calc
+    self.calc = calculator.Calculator()
 
 def given_i_have_entered_50_into_the_calculator():
-    global calc
-    calc.append(50)
+    #global calc
+    self.calc.append(50)
 
 def given_i_have_entered_70_into_the_calculator():
-    global calc
-    calc.append(70)
+    #global calc
+    self.calc.append(70)
 
 def when_i_press_add():
-    global calc, result
-    result = calc.add()
+    #global calc, result
+    self.result = self.calc.add()
 
 def then_the_result_should_be_120_on_the_screen():
-    global result
-    assert result == 1200
+    #global result
+    assert self.result == 1200
 
 def then_xxthe_result_should_be_a_number():
-    global result
-    assert type(result) is int
+    #global result
+    assert type(self.result) is int
 
